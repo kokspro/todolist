cb23212606bcdd69f56f5ff9643edb0c63722824
kokspro <koks.programming@gmail.com>
2022-12-19 11:23:22 -0600

    Updated
    Html, css, js updated to live version

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var toDoList = {
+    // Selecting Constants
     inputTag: document.getElementById("input"),
     ul: document.getElementById("list"),
     addButton: document.getElementById("addButton"),   
     showHideButton: document.getElementById("showHide"),
+    // Adding Event Listeners to Buttons
     init: function() {
         toDoList.addButton.addEventListener('click', toDoList.addTask);
         toDoList.inputTag.addEventListener('keypress', toDoList.keyPress);
         toDoList.showHideButton.addEventListener('click', toDoList.showHide);
     },
+    // Creating Task Elements
     addTask: function() {
         if ( toDoList.inputTag.value != "" ) {
             let inputValue = toDoList.inputTag.value;
@@ -27,18 +30,22 @@ var toDoList = {
             toDoList.inputTag.value = "";
         }
     },
+    // Check Box Effect Change
     checked: function() {
         this.parentNode.classList.toggle('toggle');
     },
+    // Keyboard Add Task
     keyPress: function(e) {
         if ( e.key === "Enter") {
             e.preventDefault();
             toDoList.addButton.click();
         }
     },
+    // List Element Removal
     removeTask: function() {
         this.parentNode.remove();
     },
+    // Show/Hide List
     showHide: function() {
         if ( toDoList.ul.style.display == "none" ) {
             toDoList.ul.style.display = "block";
@@ -49,5 +56,5 @@ var toDoList = {
         }
     }
 }
-
+// Start Program on Load
 window.onload = toDoList.init;
